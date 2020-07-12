c8ce37b96c44a5545da2f5cde5c317f7850a2c3e
John Leal <johnbon.leal@outlook.com>
2020-07-13 07:14:39 +0800

    feat: display image for each articles

--- src/containers/HeaderImage/HeaderImage.tsx
+++ src/containers/HeaderImage/HeaderImage.tsx
@@ -3,13 +3,19 @@ import { View, Image, Text } from 'react-native';
 import images from '../../resources/images';
 import styles from './styles';
 
-const HeaderImage = () => (
-  <View style={styles.container}>
-    <View style={styles.imageContainer}>
-      <Image source={images.placeholder} style={styles.image} />
-    </View>
-    <Text style={styles.caption}>Caption: Lorem ipsum dolor sit amet, consectetur</Text>
-  </View>
-);
+interface ComponentProps {
+  image: string | undefined;
+}
 
+const HeaderImage: React.SFC<ComponentProps> = ({ image }) => {
+  const imageDisplay = typeof image === 'string' ? { uri: image } : images.placeholder;
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={imageDisplay} style={styles.image} />
+      </View>
+      <Text style={styles.caption}>Caption: Lorem ipsum dolor sit amet, consectetur</Text>
+    </View>
+  );
+}
 export default HeaderImage;
