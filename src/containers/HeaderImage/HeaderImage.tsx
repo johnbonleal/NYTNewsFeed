e53e56c9cf00f9c25882112d2d0d2703266b05f0
John Leal <johnbon.leal@outlook.com>
2020-07-13 07:23:35 +0800

    feat: add caption for article images

--- src/containers/HeaderImage/HeaderImage.tsx
+++ src/containers/HeaderImage/HeaderImage.tsx
@@ -5,17 +5,22 @@ import styles from './styles';
 
 interface ComponentProps {
   image: string | undefined;
+  caption: string;
 }
 
-const HeaderImage: React.SFC<ComponentProps> = ({ image }) => {
+const HeaderImage: React.SFC<ComponentProps> = ({
+  image,
+  caption = 'Lorem ipsum dolor sit amet, consectetur',
+}) => {
   const imageDisplay = typeof image === 'string' ? { uri: image } : images.placeholder;
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <Image source={imageDisplay} style={styles.image} />
       </View>
-      <Text style={styles.caption}>Caption: Lorem ipsum dolor sit amet, consectetur</Text>
+      <Text style={styles.caption}>{`Caption: ${caption}`}</Text>
     </View>
   );
-}
+};
+
 export default HeaderImage;
